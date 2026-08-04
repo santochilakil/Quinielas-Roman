4d7f4b01a1c7e83843fc184e716a74209321e868
santochilakil <ayalacesar1811@icloud.com>
2026-08-03 19:35:42 -0500

    Update equipos.js

--- datos/equipos.js
+++ datos/equipos.js
@@ -1,1 +1,18 @@
+export const equipos = [
 
+  // ===== LIGA MX =====
+  {
+    id: "america",
+    nombre: "América",
+    liga: "mx",
+    escudo: "america.png"
+  },
+
+  {
+    id: "atlas",
+    nombre: "Atlas",
+    liga: "mx",
+    escudo: "atlas.png"
+  }
+
+];
